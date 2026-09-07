a6f3b0405fa3ac0102b7780e028bd71f46c15d44
Minh Nguyen <121152335+Minh111104@users.noreply.github.com>
2026-09-07 19:53:18 -0400

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> pq(stones.begin(), stones.end());
+
+        while (pq.size() > 1) {
+            int stone1 = pq.top(); // take the biggest stone from the heap
+            pq.pop();
+            int stone2 = pq.top();
+            pq.pop();
+
+            if (stone1 != stone2) {
+                pq.push(abs(stone1 - stone2));
+            }
+        }
+        return pq.empty() ? 0 : pq.top();
+    }
+};
